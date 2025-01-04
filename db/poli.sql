1c217c4b9a2c5f611c32e2cde1057e111b2c16b4
asthaadji <99196437+asthaadji@users.noreply.github.com>
2025-01-04 15:08:21 +0700

    Add files via upload

--- db/poli.sql
+++ db/poli.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 04, 2025 at 06:58 AM
+-- Generation Time: Jan 04, 2025 at 09:05 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -26,72 +26,374 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `daftar_poli`
 --
--- Error reading structure for table poli.daftar_poli: #1932 - Table &#039;poli.daftar_poli&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.daftar_poli: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`daftar_poli`&#039; at line 1
+
+CREATE TABLE `daftar_poli` (
+  `id` int(11) NOT NULL,
+  `id_pasien` int(11) NOT NULL,
+  `id_jadwal` int(11) NOT NULL,
+  `keluhan` text NOT NULL,
+  `no_antrian` int(11) DEFAULT NULL,
+  `tanggal_antrian` date NOT NULL DEFAULT curdate(),
+  `status` enum('Menunggu','Selesai') DEFAULT 'Menunggu'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `daftar_poli`
+--
+
+INSERT INTO `daftar_poli` (`id`, `id_pasien`, `id_jadwal`, `keluhan`, `no_antrian`, `tanggal_antrian`, `status`) VALUES
+(5, 8, 4, 'Sakit Pinggang', 1, '2024-12-24', 'Selesai'),
+(8, 8, 4, 'Sakit', 2, '2024-12-24', 'Selesai'),
+(9, 8, 4, 'tes', 3, '2024-12-24', 'Selesai'),
+(10, 8, 4, 'Sakit Kepala', 4, '2024-12-24', 'Selesai'),
+(11, 11, 7, 'gusi bengkak', 1, '2024-12-27', 'Menunggu'),
+(12, 11, 7, 'gigi berlubang', 2, '2024-12-27', 'Menunggu');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `detail_periksa`
 --
--- Error reading structure for table poli.detail_periksa: #1932 - Table &#039;poli.detail_periksa&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.detail_periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`detail_periksa`&#039; at line 1
+
+CREATE TABLE `detail_periksa` (
+  `id` int(11) NOT NULL,
+  `id_periksa` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_periksa`
+--
+
+INSERT INTO `detail_periksa` (`id`, `id_periksa`, `id_obat`) VALUES
+(1, 1, 3),
+(2, 2, 2),
+(3, 3, 2),
+(4, 4, 2);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `dokter`
 --
--- Error reading structure for table poli.dokter: #1932 - Table &#039;poli.dokter&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.dokter: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`dokter`&#039; at line 1
+
+CREATE TABLE `dokter` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `id_poli` int(11) NOT NULL,
+  `level` varchar(50) NOT NULL DEFAULT 'dokter'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`id`, `nama`, `alamat`, `no_hp`, `id_poli`, `level`) VALUES
+(2, 'Agus', 'Jakarta', '081209876789', 3, 'dokter'),
+(6, 'Messi', 'Jakarta', '082328986751', 2, 'dokter'),
+(8, 'adji', 'ambarawa', '085726528231', 1, 'dokter');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `jadwal_periksa`
 --
--- Error reading structure for table poli.jadwal_periksa: #1932 - Table &#039;poli.jadwal_periksa&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.jadwal_periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`jadwal_periksa`&#039; at line 1
+
+CREATE TABLE `jadwal_periksa` (
+  `id` int(11) NOT NULL,
+  `id_dokter` int(11) NOT NULL,
+  `hari` varchar(10) NOT NULL,
+  `jam_mulai` time NOT NULL,
+  `jam_selesai` time NOT NULL,
+  `status` enum('Aktif','Non Aktif') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jadwal_periksa`
+--
+
+INSERT INTO `jadwal_periksa` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`, `status`) VALUES
+(4, 2, 'Selasa', '09:00:00', '11:00:00', ''),
+(7, 6, 'Kamis', '07:00:00', '09:00:00', 'Aktif'),
+(8, 6, 'Jumat', '13:00:00', '16:00:00', ''),
+(10, 8, 'Senin', '08:00:00', '10:00:00', ''),
+(11, 8, 'senin', '15:00:00', '18:00:00', ''),
+(12, 8, 'selasa', '08:00:00', '11:00:00', 'Aktif');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `obat`
 --
--- Error reading structure for table poli.obat: #1932 - Table &#039;poli.obat&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.obat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`obat`&#039; at line 1
+
+CREATE TABLE `obat` (
+  `id` int(11) NOT NULL,
+  `nama_obat` varchar(50) NOT NULL,
+  `kemasan` varchar(35) DEFAULT NULL,
+  `harga` int(10) UNSIGNED DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id`, `nama_obat`, `kemasan`, `harga`) VALUES
+(2, 'Panadol', 'Tablet', 7000),
+(3, 'Paracetamol', 'Tablet', 10000),
+(4, 'OBH', 'botol', 30000);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `pasien`
 --
--- Error reading structure for table poli.pasien: #1932 - Table &#039;poli.pasien&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.pasien: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`pasien`&#039; at line 1
+
+CREATE TABLE `pasien` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_ktp` varchar(20) NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `no_rm` char(10) DEFAULT NULL,
+  `level` varchar(50) NOT NULL DEFAULT 'pasien'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `nama`, `alamat`, `no_ktp`, `no_hp`, `no_rm`, `level`) VALUES
+(8, 'adji', 'Semarang', '3326161301940001', '08232970064', '202412-1', 'pasien'),
+(10, 'sopo', 'Semarang', '3091098712346789', '08232970064', '202412-002', 'pasien'),
+(11, 'astha', 'ambarawa', '12345', '12345', '202412-3', 'pasien'),
+(12, 'astha', 'semarang', '12345', '12345', '202412-4', 'pasien');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `periksa`
 --
--- Error reading structure for table poli.periksa: #1932 - Table &#039;poli.periksa&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`periksa`&#039; at line 1
+
+CREATE TABLE `periksa` (
+  `id` int(11) NOT NULL,
+  `id_daftar_poli` int(11) NOT NULL,
+  `tgl_periksa` date NOT NULL,
+  `catatan` text NOT NULL,
+  `biaya_periksa` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `id_daftar_poli`, `tgl_periksa`, `catatan`, `biaya_periksa`) VALUES
+(1, 5, '2024-12-24', 'Kurangi Begadang', 160000),
+(2, 10, '2024-12-24', 'Jangan Banyak Begadang', 155000),
+(3, 8, '2024-12-27', 'asdadaa', 157000),
+(4, 9, '2024-12-27', 'asdadaa', 157000);
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `poli`
 --
--- Error reading structure for table poli.poli: #1932 - Table &#039;poli.poli&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.poli: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`poli`&#039; at line 1
+
+CREATE TABLE `poli` (
+  `id` int(11) NOT NULL,
+  `nama_poli` varchar(25) NOT NULL,
+  `keterangan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `poli`
+--
+
+INSERT INTO `poli` (`id`, `nama_poli`, `keterangan`) VALUES
+(1, 'Poli Umum', 'Poli Umum'),
+(2, 'Poli Gigi', 'Layanan kesehatan yang berfokus pada diagnosis, pengobatan, dan pencegahan masalah kesehatan gigi dan mulut. Poli ini menangani berbagai keluhan seperti gigi berlubang, sakit gigi, penyakit gusi, pemasangan kawat gigi, hingga pembersihan karang gigi..'),
+(3, 'Poli Anak', 'Poli Anak');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tb_admin`
 --
--- Error reading structure for table poli.tb_admin: #1932 - Table &#039;poli.tb_admin&#039; doesn&#039;t exist in engine
--- Error reading data for table poli.tb_admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`tb_admin`&#039; at line 1
+
+CREATE TABLE `tb_admin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `username` varchar(35) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `level` enum('admin','dokter','pasien') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id`, `nama`, `username`, `password`, `level`) VALUES
+(1, 'admin', 'admin', '123', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pasien` (`id_pasien`),
+  ADD KEY `id_jadwal` (`id_jadwal`);
+
+--
+-- Indexes for table `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_periksa` (`id_periksa`),
+  ADD KEY `id_obat` (`id_obat`);
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_poli` (`id_poli`);
+
+--
+-- Indexes for table `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_dokter` (`id_dokter`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_daftar_poli` (`id_daftar_poli`);
+
+--
+-- Indexes for table `poli`
+--
+ALTER TABLE `poli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `periksa`
+--
+ALTER TABLE `periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `poli`
+--
+ALTER TABLE `poli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  ADD CONSTRAINT `daftar_poli_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`),
+  ADD CONSTRAINT `daftar_poli_ibfk_2` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal_periksa` (`id`);
+
+--
+-- Constraints for table `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  ADD CONSTRAINT `detail_periksa_ibfk_1` FOREIGN KEY (`id_periksa`) REFERENCES `periksa` (`id`),
+  ADD CONSTRAINT `detail_periksa_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`);
+
+--
+-- Constraints for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD CONSTRAINT `dokter_ibfk_1` FOREIGN KEY (`id_poli`) REFERENCES `poli` (`id`);
+
+--
+-- Constraints for table `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  ADD CONSTRAINT `jadwal_periksa_ibfk_1` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id`);
+
+--
+-- Constraints for table `periksa`
+--
+ALTER TABLE `periksa`
+  ADD CONSTRAINT `periksa_ibfk_1` FOREIGN KEY (`id_daftar_poli`) REFERENCES `daftar_poli` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
