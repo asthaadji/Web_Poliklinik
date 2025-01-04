981bc7acf7c25a1a235f793f8bc60af6e1c22843
asthaadji <99196437+asthaadji@users.noreply.github.com>
2025-01-04 12:59:13 +0700

    Add files via upload

---
+++ db/poli.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 04, 2025 at 06:58 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `poli`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_poli`
+--
+-- Error reading structure for table poli.daftar_poli: #1932 - Table &#039;poli.daftar_poli&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.daftar_poli: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`daftar_poli`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_periksa`
+--
+-- Error reading structure for table poli.detail_periksa: #1932 - Table &#039;poli.detail_periksa&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.detail_periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`detail_periksa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+-- Error reading structure for table poli.dokter: #1932 - Table &#039;poli.dokter&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.dokter: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`dokter`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_periksa`
+--
+-- Error reading structure for table poli.jadwal_periksa: #1932 - Table &#039;poli.jadwal_periksa&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.jadwal_periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`jadwal_periksa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+-- Error reading structure for table poli.obat: #1932 - Table &#039;poli.obat&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.obat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`obat`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+-- Error reading structure for table poli.pasien: #1932 - Table &#039;poli.pasien&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.pasien: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`pasien`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `periksa`
+--
+-- Error reading structure for table poli.periksa: #1932 - Table &#039;poli.periksa&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`periksa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `poli`
+--
+-- Error reading structure for table poli.poli: #1932 - Table &#039;poli.poli&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.poli: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`poli`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+-- Error reading structure for table poli.tb_admin: #1932 - Table &#039;poli.tb_admin&#039; doesn&#039;t exist in engine
+-- Error reading data for table poli.tb_admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poli`.`tb_admin`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
